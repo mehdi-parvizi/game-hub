0db0e41f5ea1a9194aa628c89563ab35019d2b5b
Mehdi <aceparvizi@gmail.com>
2024-01-14 12:06:16 +0100

    Install Chakra ui

--- game-hub/src/App.tsx
+++ game-hub/src/App.tsx
@@ -1,2 +1,9 @@
-function App() {}
+import { Button, ButtonGroup } from "@chakra-ui/react";
+function App() {
+  return (
+    <div className="">
+      <Button colorScheme="blue">Button</Button>
+    </div>
+  );
+}
 export default App;
