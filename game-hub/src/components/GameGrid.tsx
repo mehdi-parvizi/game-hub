4085392ab3f61f94dd470c5bf4c55f63a62236a0
Mehdi <aceparvizi@gmail.com>
2024-01-15 17:50:02 +0100

    Show a spinner while fetching genres

--- game-hub/src/components/GameGrid.tsx
+++ game-hub/src/components/GameGrid.tsx
@@ -19,13 +19,11 @@ const GameGrid = () => {
         {isLoading &&
           skeletons.map((skeleton) => (
             <GameCardContainer>
-              {" "}
               <GameCardSkeleton key={skeleton} />{" "}
             </GameCardContainer>
           ))}
         {data.map((game) => (
           <GameCardContainer>
-            {" "}
             <GameCard key={game.id} game={game} />
           </GameCardContainer>
         ))}
