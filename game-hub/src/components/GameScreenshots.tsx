66242d890b4b804e95e9ce31209f846e3f9e1732
Mehdi <aceparvizi@gmail.com>
2024-03-08 15:43:25 +0100

    Build game screenshots

---
+++ game-hub/src/components/GameScreenshots.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import useScreenshots from "../hooks/useScreenShots";
+import { Image, SimpleGrid } from "@chakra-ui/react";
+
+interface Props {
+  gameId: number;
+}
+
+const GameScreenshots = ({ gameId }: Props) => {
+  const { data, isLoading, error } = useScreenshots(gameId);
+  console.log(data);
+
+  if (isLoading) return null;
+  if (error) throw error;
+
+  return (
+    <SimpleGrid spacing={2} columns={{ base: 1, md: 2 }}>
+      {data?.results.map((screenshot) => (
+        <Image key={screenshot.id} src={screenshot.image} />
+      ))}
+    </SimpleGrid>
+  );
+};
+
+export default GameScreenshots;
---
+++ game-hub/src/entities/Screenshot.ts
@@ -0,0 +1,6 @@
+export interface Screenshot {
+  id: number;
+  width: number;
+  height: number;
+  image: string;
+}
